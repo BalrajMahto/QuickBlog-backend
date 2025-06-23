d9ac4e8df2541256aed9a95be71683c480696041
deno <mailzeni42@gmail.com>
2025-06-23 19:20:22 +0530

    Auth middleware fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
+
+    const token = authHeader.split(' ')[1];
 
     try{
         jwt.verify(token, process.env.JWT_SECRET);
